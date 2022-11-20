1bd4b39c42240704b05057e2cc7e659cc73738f2
HugMunn <artur.popov.wrk@gmail.com>
2022-11-20 13:39:07 +0200

    The project setup for automation

---
+++ Project setup/Setup/Setup.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import time
+
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")
+options.add_argument("--incognito")
+
+# Add the webdriver to the PATH and you don't need to clarify the way(path) to the file
+driver = webdriver.Chrome(options=options)
+driver.implicitly_wait(5)
+
+driver.get("https://rozetka.com.ua/ua/")
+time.sleep(10)
+
+driver.quit()
